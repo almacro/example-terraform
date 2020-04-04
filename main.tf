585851c699120afe16bf2781f2d9462b0ddc9b14
Cloud Developer <moheybear@gmail.com>
2020-04-04 11:43:45 +0000

    Fix up Terraform main to use working VPC

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 resource "aws_instance" "example" {
-  ami           = "ami-014a2e30da708ee8b"
-  instance_type = "t3.nano"
+  ami                         = "ami-0ce21b51cb31a48b8"
+  instance_type               = "t3.nano"
+  key_name                    = "mocn-ng"
+
+  subnet_id                   = "subnet-d3d796aa"
+  security_groups             = ["sg-9e2db4ee"]
+  associate_public_ip_address = true
 
   tags = {
     Name = "terraform-example"
