90a3919dd2b1f348349a8f8f4f69b527e352549b
Cloud Developer <moheybear@gmail.com>
2020-04-06 16:00:07 +0000

    Flatten IPv4 output from list to single value

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
-  description = "Public IP address assigned to the instance"
-  value       = aws_instance.example.*.public_ip
+    value = aws_instance.example.public_ip
+    description = "The public IP address of the web server"
 }
