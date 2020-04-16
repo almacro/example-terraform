b71a80715fd240473a9ad4a7176f5804cf469521
Cloud Developer <moheybear@gmail.com>
2020-04-16 05:17:54 +0000

    Replace single EC2 instance with autoscaled group

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-  vpc_id = "vpc-324ba04a"
+  name   = "terraform-example-instance"
+  vpc_id = data.aws_vpc.selected.id
+
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
@@ -21,13 +22,12 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0ce21b51cb31a48b8"
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-0ce21b51cb31a48b8"
   instance_type = "t3.nano"
   key_name      = "mocn-ng"
 
-  subnet_id                   = "subnet-d3d796aa"
-  vpc_security_group_ids      = [aws_security_group.instance.id]
+  security_groups             = [aws_security_group.instance.id]
   associate_public_ip_address = true
 
   user_data = <<-EOF
@@ -46,7 +46,98 @@ resource "aws_instance" "example" {
               nohup /var/www/server.sh &
               EOF
 
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.selected.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.selected.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.selected.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name   = "terraform-example-alb"
+  vpc_id = data.aws_vpc.selected.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
+#output "public_ip" {
+#  description = "Public IP address assigned to the instance"
+#  value       = aws_instance.example.*.public_ip
+#}
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
